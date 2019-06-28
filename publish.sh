9994d3007e76b1c24be4af716619a744435f7e2f
michaeldeongreen <michaeldeongreen@gmail.com>
2019-06-28 18:16:59 -0500

    Updated root files with name change

--- publish.sh
+++ publish.sh
@@ -1,9 +1,9 @@
-"c:/Git/Blog.GrenitausConsulting\gc-cli.sh" build -prod
+"c:/Git/Blog.Michaeldeongreen\gc-cli.sh" build -prod
 
 rm -rf "c:/temp/My Blog Uploads/dist/api" && mkdir "c:/temp/My Blog Uploads/dist/api"
-dotnet publish "C:\Git\Blog.GrenitausConsulting\WebApi\Blog.GrenitausConsulting.Core.Web.Api" -c Release -o "C:\temp\My Blog Uploads\dist\api"
+dotnet publish "C:\Git\Blog.Michaeldeongreen\WebApi\Blog.Michaeldeongreen.Core.Web.Api" -c Release -o "C:\temp\My Blog Uploads\dist\api"
 
-cd c:/Git/Blog.GrenitausConsulting/WebClient
+cd c:/Git/Blog.Michaeldeongreen/WebClient
 ng build --prod --output-path "C:\temp\My Blog Uploads\dist\client"
 
 exit 0
