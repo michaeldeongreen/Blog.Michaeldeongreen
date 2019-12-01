83e7ed9711af123b017a6849e95f0809f2b4ebe1
michaeldeongreen <michaeldeongreen@gmail.com>
2019-12-01 12:23:58 -0600

    Fixed issue with unsafe urls in angular

--- WebClient/src/app/preview/preview.component.ts
+++ WebClient/src/app/preview/preview.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Pipe } from '@angular/core';
 import { HttpService } from '../http.service';
 import { ActivatedRoute } from '@angular/router';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-preview',
@@ -11,7 +11,7 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Pipe({ name: 'SafeHtml' })
 export class PreviewComponent implements OnInit {
-    html: SafeHtml;
+    html: string;
     parameter: string;
     item: any = {};
     busy: boolean = true;
@@ -42,8 +42,7 @@ export class PreviewComponent implements OnInit {
         this.httpService.getPostPreviewHtml(title)
             .subscribe(data => {
                 // get pager object from service
-                this.html = this.domSanitizer.bypassSecurityTrustHtml(data);
-                console.log(this.html);
+                this.html = data;
                 this.busy = false;
             },
             err => {
